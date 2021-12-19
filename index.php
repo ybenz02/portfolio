cce196ddf1ca69e82fed6786aacb0aa27b3d6719
ybenz02 <ybenzerroug2@gmail.com>
2021-12-19 13:57:41 +0100

    ok

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="utf-8" />
+        <title>page title</title>
+        
+        <link rel="stylesheet" type="text/css" href="" /><!-- external CSS goes below -->
+        
+    </head>
+    <body>
+        
+        
+        <script type="text/javascript" src=""></script><!-- external JS goes below -->
+        
+    </body>
+</html>
